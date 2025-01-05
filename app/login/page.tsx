73f3e51d8337f8802c844d6eb29e58d13f294108
Ariel Elias <eliasariel@live.com>
2025-01-04 22:20:57 -0300

    first commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,9 @@
-
+import { AuthForm } from "@/components/auth/auth-form"
 
 const LoginPage = () => {
   return (
     <section className="container mx-auto mt-32 lg:mt-44 px-3">
+        <AuthForm />
     </section>
   )
 }
